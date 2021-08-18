d69c6b11b70c9d92a8b1c611d083d38e0b2444e5
Max Kassander <maximilian.kassander@gmail.com>
2021-08-18 23:01:20 +0200

    Look at mouse

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,24 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody Rigidbody => GetComponent<Rigidbody>();
 
     public float speed;
+    private Vector2 MousePos;
+    
 
     void FixedUpdate()
     {
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical"));
         Rigidbody.MovePosition(transform.position + input * (Time.deltaTime * speed));
+
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+ 
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.cyan);
+ 
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
     }
 }
